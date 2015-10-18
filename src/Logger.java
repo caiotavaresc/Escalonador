6ff88bd5638334bde4417f200c55f496082bcfca
GallienTramp <sousa_giovani@hotmail.com>
2015-10-17 22:09:46 -0300

    Nova classe que constroi os logs

--- src/Logger.java
+++ src/Logger.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 public class Logger {
-    FileWriter l;
+    private FileWriter l;
     public Logger(){
         try {
             l = new FileWriter(String.format("%02d", Escalonador._quantum)+".txt");
